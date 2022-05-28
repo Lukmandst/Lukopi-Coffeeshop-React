6666a47e68bb314fb4a075f7943230169de6fba5
Lukmandst <lukmandwi11@gmail.com>
2022-05-29 05:10:36 +0700

    forgot pass page done

--- src/pages/ForgotPass/index.js
+++ src/pages/ForgotPass/index.js
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
 import "./forgotpass.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Footer from "../../components/Footer";
 
 export class ForgotPass extends Component {
   render() {
-    return <div>ForgotPass</div>;
+    return (
+      <div>
+        <section className="container-fluid">
+          <div className="row forgot-row">
+            <div className="col forgot-wrapper d-flex">
+              <header className="forgot-header">
+                <h1>Forgot your password?</h1>
+                <p className="sub-forgot"> Don't worry, we got your back!</p>
+              </header>
+              <form className="send-email-form d-flex">
+                <input type="email" placeholder="Enter your email adress to get link"></input>
+                <button className="send-btn">Send</button>
+              </form>
+              <section>
+                <p className="resend-info">
+                  Click here if you didn't receive any link in 2 minutes
+                </p>
+                <button className="resend-btn">Resend Link</button>
+                <p>timer</p>
+              </section>
+            </div>
+          </div>
+        </section>
+        <Footer />
+      </div>
+    );
   }
 }
 
