4aed3c866937e820b556dabce5dde43ac14149c2
Lukmandst <lukmandwi11@gmail.com>
2022-05-28 18:58:18 +0700

    classname

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,18 +2,19 @@ import{Link} from "react-router-dom"
 
 import "../../pages/SignIn/signIn.css";
 import Footer from "../../components/Footer";
+import "./signIn.css"
 
 function SignIn() {
   return (
     <div>
-      <section class="login container d-inline">
-        <div class="row">
-          <div class="login-left col-md-6 col-lg-6 d-flex  justify-content-lg-end align-items-lg-end"></div>
-          <div class="login-right col-md-6">
-            <div class="container home">
-              <div class="brand d-flex align-items-center">
+      <section className="login container d-inline">
+        <div className="row">
+          <div className="login-left col-md-6 col-lg-6 d-flex  justify-content-lg-end align-items-lg-end"></div>
+          <div className="login-right col-md-6">
+            <div className="container home">
+              <div className="brand d-flex align-items-center">
                 <div>
-                  <a href="/" class="brand-logo">
+                  <a href="/" className="brand-logo">
                     <img
                       src="/assets/image/stock/coffee 1.png"
                       alt="LukopiLogo"
@@ -24,41 +25,41 @@ function SignIn() {
               </div>
               <div>
                 <a href="/signUp">
-                  <button class="signup">Sign Up</button>
+                  <button className="signup">Sign Up</button>
                 </a>
               </div>
             </div>
-            <div class="col login-form-wrapper">
-              <div class="col">
-                <div class="header text-center">
+            <div className="col login-form-wrapper">
+              <div className="col">
+                <div className="header text-center">
                   <h2>Login</h2>
                 </div>
-                <div class="login-form">
-                  <label for="Email" class="form-label">
+                <div className="login-form">
+                  <label for="Email" className="form-label">
                     Email address :{" "}
                   </label>
                   <input
                     type="email"
-                    class="form-control"
+                    className="form-control"
                     id="Email"
                     placeholder="Enter your email address"
                   />
-                  <label for="password" class="form-label">
+                  <label for="password" className="form-label">
                     Password :{" "}
                   </label>
                   <input
                     type="password"
-                    class="form-control"
+                    className="form-control"
                     id="password"
                     placeholder="Enter your password"
                   />
                   <a href="/forgot">Forgot password?</a>
-                  <button class="signin">Login</button>
-                  <button class="signin-google">
+                  <button className="signin">Login</button>
+                  <button className="signin-google">
                     <img
                       src="/assets/image/stock/google-icon.png"
                       alt="google"
-                      class="google-btn"
+                      className="google-btn"
                     />
                     Login with Google
                   </button>
@@ -68,20 +69,20 @@ function SignIn() {
           </div>
         </div>
       </section>
-      <section class="card-wrapper d-none d-lg-block">
-        <div class="card justify-content-center align-items-center">
-          <div class="card-body d-flex list-style-none">
+      <section className="card-wrapper d-none d-lg-block">
+        <div className="card justify-content-center align-items-center">
+          <div className="card-body d-flex list-style-none">
             <ul>
               <li>
-                <h4 class="card-title">Get your member card now!</h4>
+                <h4 className="card-title">Get your member card now!</h4>
               </li>
               <li>
-                <p class="card-text">
+                <p className="card-text">
                   Let's join with our member and enjoy the deals.
                 </p>
               </li>
             </ul>
-            <a href="#" class="btn-create d-flex">
+            <a href="#" className="btn-create d-flex">
               Create Now
             </a>
           </div>
