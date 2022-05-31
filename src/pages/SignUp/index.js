3ce48f2305a6e4074f71d3b227ee63d73cae9d8f
Lukmandst <lukmandwi11@gmail.com>
2022-05-31 08:06:44 +0700

    alert success and fail but have one bug on catch another error

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -3,16 +3,20 @@ import Footer from "../../components/Footer";
 import "./signUp.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import Googleimg from "../../assets/image/stock/google-icon.png"
+import Googleimg from "../../assets/image/stock/google-icon.png";
 import CardMember from "../../components/CardMember";
-import Logo from "../../assets/image/stock/coffee 1.png"
-
+import Logo from "../../assets/image/stock/coffee 1.png";
 
 class SignUp extends Component {
   state = {
     email: "",
     pass: "",
     phone: "",
+    isSuccess: false,
+    isError: false,
+    succesmsg: "",
+    errormsg: [],
+    errormsgone: "",
   };
   render() {
     return (
@@ -25,10 +29,7 @@ class SignUp extends Component {
                 <div className="brand d-flex align-items-center">
                   <div>
                     <Link to="/" className="brand-logo">
-                      <img
-                        src={Logo}
-                        alt="LukopiLogo"
-                      />
+                      <img src={Logo} alt="LukopiLogo" />
                     </Link>
                   </div>
                   <Link to="/">Lukopi</Link>
@@ -44,7 +45,7 @@ class SignUp extends Component {
                   <div className="header text-center">
                     <h2>Sign Up</h2>
                   </div>
-                  <div className="login-form">
+                  <form className="login-form">
                     <label htmlFor="Email" className="form-label">
                       Email address :{" "}
                     </label>
@@ -86,11 +87,41 @@ class SignUp extends Component {
                           phone: e.target.value,
                         });
                       }}
-                    />
+                    />{" "}
+                    <br />
+                    {this.state.isSuccess ? (
+                      <div className="alert alert-success" role="alert">
+                        {this.state.succesmsg}
+                        {/* <Navigate to="/" /> */}
+                      </div>
+                    ) : (
+                      <></>
+                    )}
+                    {this.state.isError &&
+                    Array.isArray(this.state.errormsg) ? (
+                      this.state.errormsg.map((erroritem) => (
+                        <div
+                          key={erroritem.param}
+                          className="alert alert-danger"
+                          role="alert"
+                        >
+                          {erroritem.msg}
+                        </div>
+                      ))
+                    ) : (
+                      <></>
+                    )}
+                    {this.state.isError ? (
+                      <div className="alert alert-danger" role="alert">
+                        {this.state.errormsgone}
+                      </div>
+                    ) : (
+                      <></>
+                    )}
                     <button
                       className="signin"
                       onClick={(e) => {
-                        e.preventDefault()
+                        e.preventDefault();
                         const { email, pass, phone } = this.state;
                         const body = {
                           email,
@@ -99,13 +130,27 @@ class SignUp extends Component {
                         };
                         axios
                           .post("http://localhost:8080/auth/signup", body)
-                          .then((result) => console.log(result.data))
-                          .catch((error) => {
-                            let dataError = error.response.data.error; //many errors in array
-                            dataError.forEach((item) => { // show every errors 
-                              console.log(item.msg);
+                          .then((result) => {
+                            console.log(result.data);
+                            this.setState({
+                              succesmsg: result.data.msg,
+                              isSuccess: true,
+                              isError: false,
                             });
-                          });
+                          })
+                          .catch((error) => {
+                            let dataError = error.response.data.error;
+                            console.log(dataError); //many errors in array
+                            this.setState({
+                              isError: true,
+                              errormsg: dataError,
+                              // errormsgone: oneError,
+                            });
+                          })
+                          // .catch((error) => {
+                          //   let oneError = error;
+                          //   console.log(oneError);
+                          // });
                       }}
                     >
                       Sign Up
@@ -118,13 +163,13 @@ class SignUp extends Component {
                       />
                       Sign Up with Google
                     </button>
-                  </div>
+                  </form>
                 </div>
               </div>
             </div>
           </div>
         </section>
-        <CardMember/>
+        <CardMember />
         <Footer />
       </div>
     );
