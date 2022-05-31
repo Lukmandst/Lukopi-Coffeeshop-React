1e0c22b7aa0887ec2c407307e87cd24400caf13b
Lukmandst <lukmandwi11@gmail.com>
2022-05-31 08:06:04 +0700

    alert success and fail

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import axios from "axios";
 
 import "../../pages/SignIn/signIn.css";
@@ -7,14 +7,20 @@ import Footer from "../../components/Footer";
 import "./signIn.css";
 import Googleimg from "../../assets/image/stock/google-icon.png";
 import CardMember from "../../components/CardMember";
-import Logo from "../../assets/image/stock/coffee 1.png"
+import Logo from "../../assets/image/stock/coffee 1.png";
 
 class SignIn extends Component {
   state = {
     email: "",
     pass: "",
+    isSuccess: false,
+    isError: false,
+    errormsg: "",
   };
   render() {
+    // if (this.state.isSuccess === true) {
+    //   return <Navigate to="/" />;
+    // }
     return (
       <div>
         <section className="container-fluid">
@@ -25,10 +31,7 @@ class SignIn extends Component {
                 <div className="brand d-flex align-items-center">
                   <div>
                     <Link to="/" className="brand-logo">
-                      <img
-                        src={Logo}
-                        alt="LukopiLogo"
-                      />
+                      <img src={Logo} alt="LukopiLogo" />
                     </Link>
                   </div>
                   <Link to="/">Lukopi</Link>
@@ -73,7 +76,23 @@ class SignIn extends Component {
                         });
                       }}
                     />
-                    <Link to="/forgot">Forgot password?</Link>
+                    <Link to="/forgot">Forgot password?</Link> <br />
+                    
+                    {this.state.isSuccess ? (
+                      <div className="alert alert-success" role="alert">
+                        Login Success
+                        {/* <Navigate to="/" /> */}
+                      </div>
+                    ) : (
+                      <></>
+                    )}
+                    {this.state.isError ? (
+                      <div className="alert alert-danger" role="alert">
+                        {this.state.errormsg}
+                      </div>
+                    ) : (
+                      <></>
+                    )}
                     <button
                       className="signin"
                       onClick={(e) => {
@@ -91,9 +110,16 @@ class SignIn extends Component {
                               "userinfo",
                               JSON.stringify(result.data.data)
                             );
+                            this.setState({
+                              isSuccess: true,
+                            });
                           })
                           .catch((error) => {
                             console.log(error.response.data.err.msg); //show error msg
+                            this.setState({
+                              isError: true,
+                              errormsg: `${error.response.data.err.msg}`,
+                            });
                           });
                       }}
                     >
