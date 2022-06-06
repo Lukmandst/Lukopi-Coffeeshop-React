8e120502066265189d690216ce1b1af631d3eff4
Lukmandst <lukmandwi11@gmail.com>
2022-06-06 09:36:06 +0700

    mini update

--- src/pages/Payment/index.js
+++ src/pages/Payment/index.js
@@ -9,8 +9,12 @@ class Payment extends Component {
   state = {
     cartProduct: JSON.parse(localStorage.getItem("usercart")),
     isSuccess: false,
+    products: [],
+    tax: 0,
+    Bill: 0,
+    userAddress: "",
+    userphone: "",
   };
-
 
   handlerCreateTransaction = (e) => {
     e.preventDefault();
@@ -36,7 +40,44 @@ class Payment extends Component {
       })
       .catch((error) => console.error(error));
   };
+
+  async componentDidMount() {
+    let product_id = this.state.cartProduct.id;
+    const url = `http://localhost:8080/product?id=${product_id}`;
+    try {
+      const result = await axios.get(url);
+      const productsArray = result.data.data;
+      console.log(productsArray[0]);
+
+      const { token } = JSON.parse(localStorage.getItem("userinfo"));
+      const config = {
+        headers: { Authorization: `Bearer ${token}` },
+      };
+
+      const userResult = await axios.get(
+        "http://localhost:8080/user/info",
+        config
+      );
+      console.log(userResult.data.data[0]);
+      this.setState({
+        products: productsArray[0],
+        userAddress: userResult.data.data[0].delivery_address,
+        userphone: userResult.data.data[0].phone_number,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+    let tax = parseInt(this.state.products.price) * 0.1;
+    let Bill = tax + parseInt(this.state.products.price);
+    console.log(Bill);
+    this.setState({
+      tax: tax,
+      Bill: Bill,
+    });
+  }
+
   render() {
+    const { products, Bill, tax, userAddress, userphone } = this.state;
     return (
       <div>
         <Navbar login={true} />
@@ -48,15 +89,37 @@ class Payment extends Component {
               </header>
               <div className="order-wrapper">
                 <h3>Order Summary</h3>
-                <div className="product-wrapper"></div>
+                <br/>
+                <div className="product-wrapper d-flex">
+                  <div className="img-prdct">
+                    <img
+                      src={`http://localhost:8080${products.image}`}
+                      alt="product-icon"
+                    />
+                  </div>
+                  <div className="product-details-payment">
+                    <header>{products.name}</header>
+                    <p className="size-item-payment">
+                      <span className="amount-item-payment">
+                        x{this.state.cartProduct.quantity}
+                        <br />
+                      </span>
+                      ({this.state.cartProduct.size})
+                    </p>
+                  </div>
+                  <div className="payment-prdct-price">
+                    IDR {products.price}
+                  </div>
+                </div>
+                <br/>
                 <div className="fee-wrapper">
                   <div className="subtotal d-flex">
                     <p>SUBTOTAL</p>
-                    <p className="fee">IDR 120000</p>
+                    <p className="fee">IDR {products.price}</p>
                   </div>
                   <div className="taxandfee d-flex">
                     <p>TAX & FEE</p>
-                    <p className="fee">IDR 20000</p>
+                    <p className="fee">IDR {tax}</p>
                   </div>
                   <div className="shipping d-flex">
                     <p>SHIPPING</p>
@@ -64,7 +127,7 @@ class Payment extends Component {
                   </div>
                   <div className="total d-flex">
                     <p>TOTAL</p>
-                    <p className="fee">IDR 150000</p>
+                    <p className="fee">IDR {Bill + 10000}</p>
                   </div>
                 </div>
               </div>
@@ -80,10 +143,8 @@ class Payment extends Component {
                     {" "}
                     <span>Delivery</span> to
                   </p>
-                  <p className="address-details">
-                    Km 5 refinery road oppsite re public road, effurun, Jakarta
-                  </p>
-                  <p className="phone-number">0-90099898</p>
+                  <p className="address-details">{userAddress}</p>
+                  <p className="phone-number">{userphone}</p>
                 </section>
               </div>{" "}
               <br />
@@ -91,19 +152,19 @@ class Payment extends Component {
                 <h4>Payment method</h4>
                 <form className="payment-form">
                   <div className="input-form d-flex">
-                    <input type="radio" id="creditcard"></input>
+                    <input type="radio" id="creditcard" name="payment"></input>
                     <div className="label-icon creditcard"></div>
                     <label htmlFor="creditcard">Card</label>
                   </div>
                   <br />
                   <div className="input-form d-flex">
-                    <input type="radio" id="bank-acc"></input>
+                    <input type="radio" id="bank-acc" name="payment"></input>
                     <div className="label-icon bank"></div>
                     <label htmlFor="bank-acc">Bank Account</label>
                   </div>
                   <br />
                   <div className="input-form d-flex">
-                    <input type="radio" id="cod"></input>
+                    <input type="radio" id="cod" name="payment"></input>
                     <div className="label-icon cod"></div>
                     <label htmlFor="cod">Cash On Delivery</label>
                   </div>
