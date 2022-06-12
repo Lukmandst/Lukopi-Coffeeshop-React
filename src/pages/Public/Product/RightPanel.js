3099cc2939cf500d704a226cf23282a288be7bf5
Lukmandst <lukmandwi11@gmail.com>
2022-06-12 22:35:53 +0700

    move everything into redux! (sort not working)

--- src/pages/Public/Product/RightPanel.js
+++ src/pages/Public/Product/RightPanel.js
@@ -1,28 +1,37 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { DropdownButton, Dropdown } from "react-bootstrap";
 import Pagination from "../../../components/Pagination";
 import ProductIcon from "../../../components/ProductIcon";
 import ProductIconAdmin from "../../../components/ProductIconAdmin";
+import {
+  getCoffee,
+  getFoods,
+  getListProducts,
+  getNonCoffe,
+} from "../../../Redux/actions/productActions";
+import { useSearchParams } from "react-router-dom";
 
 function RightPanel() {
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage] = useState(5);
 
+  const [sort, setSort] = useState("ASC");
+  const [_, setSearchParams] = useSearchParams();
 
+  const dispatch = useDispatch();
   const { getAllProductsResult, getAllProductsLoading, getAllProductsError } =
     useSelector((state) => state.ProductReducer);
-  
-    // Get current product
+
+  // Get current product
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = getAllProductsResult.slice(
     indexOfFirstPost,
     indexOfLastPost
-  
-    );
+  );
   // Change page
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
-  
   return (
     <>
       <div className="col right-panel">
@@ -33,8 +42,10 @@ function RightPanel() {
                 <div
                   className="active"
                   to="/products"
-                  onClick={() => {
-                    // setIsAllMenu(true);
+                  onClick={(e) => {
+                    e.preventDefault();
+                    dispatch(getListProducts(sort));
+                    setSearchParams();
                   }}
                 >
                   All Menu
@@ -45,7 +56,9 @@ function RightPanel() {
                   // to="?category=07e88ba9-1a54-46ab-bf2c-3dc8831090a4"
                   className="menu-link"
                   onClick={(e) => {
-                    // setIsCoffee(true);
+                    e.preventDefault();
+                    dispatch(getCoffee(sort));
+                    setSearchParams({ category: "Coffee" });
                   }}
                 >
                   Coffee
@@ -55,8 +68,10 @@ function RightPanel() {
                 <div
                   to="?category=30b95dde-a820-41dd-b474-902026e3e755"
                   className="menu-link"
-                  onClick={() => {
-                    // setIsNonCoffee(true);
+                  onClick={(e) => {
+                    e.preventDefault();
+                    dispatch(getNonCoffe(sort));
+                    setSearchParams({ category: "NonCoffee" });
                   }}
                 >
                   Non Coffee
@@ -66,23 +81,44 @@ function RightPanel() {
                 <div
                   to="?category=ea71bfcd-f1f1-4976-ae1e-9ff0f2c70d0e"
                   className="menu-link"
-                  onClick={() => {
-                    // setIsFoods(true);
+                  onClick={(e) => {
+                    e.preventDefault();
+                    dispatch(getFoods(sort));
+                    setSearchParams({ category: "Foods" });
                   }}
                 >
                   Foods
                 </div>
               </li>
               <li className="menu-item">
-                <div to="#" className="menu-link">
-                  Add-on
-                </div>
+                <DropdownButton
+                  id="dropdown-basic-button"
+                  variant="Secondary"
+                  title="Sort"
+                >
+                  <Dropdown.Item
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setSort("ASC");
+                    }}
+                  >
+                    A-Z
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    onChange={(e) => {
+                      e.preventDefault();
+                      setSort("DESC");
+                    }}
+                  >
+                    Z-A
+                  </Dropdown.Item>
+                </DropdownButton>
               </li>
             </ul>
           </div>
           <div className="show-product">
             <div className=" product-wrapper d-flex">
-              {Array.isArray(currentPosts) ? (
+              {currentPosts ? (
                 currentPosts.map((product) => (
                   <ProductIcon key={product.id} data={product} />
                 ))
@@ -119,7 +155,7 @@ function RightPanel() {
             totalPosts={getAllProductsResult.length}
             paginate={paginate}
           />
-          <br/>
+          <br />
           <button className="add-product-btn-admin">Add New Product</button>
         </div>
       </div>
--- src/pages/Public/Product/index.js
+++ src/pages/Public/Product/index.js
@@ -1,78 +1,21 @@
-import React, { useEffect, useState } from "react";
-import Footer from "../../../components/Footer";
-import Navbar from "../../../components/Navbar/Navbar";
-import "./product.css";
-import axios from "axios";
-import { useSearchParams } from "react-router-dom";
-
-import withParams from "../../../helper/WithParams";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getListProducts } from "../../../Redux/actions/productActions";
-import LeftPanel from "./LeftPanel";
+
+import Footer from "../../../components/Footer";
+import Navbar from "../../../components/Navbar/Navbar";
+import withParams from "../../../helper/WithParams";
 import RightPanel from "./RightPanel";
+import LeftPanel from "./LeftPanel";
+import "./product.css";
+
 
 function Product() {
-  const [products, setProduct] = useState([]);
-
-  const [searchParams, setSearchParams] = useSearchParams();
   //dispatch
   const dispatch = useDispatch();
 
-
-  const handleForCoffee = () => {
-    searchParams.get({ category: "coffee" });
-    const parameter = "?category=07e88ba9-1a54-46ab-bf2c-3dc8831090a4";
-    const url = `http://localhost:8080/product${parameter}`;
-    axios
-      .get(url)
-      .then((result) => {
-        // console.log(result.data.data);
-        setProduct(result.data.data);
-        setSearchParams({ category: "Coffee" });
-      })
-      .catch((error) => console.error(error));
-    // <Link to={`${parameter}`} />;
-  };
-  const handleForNonCoffee = () => {
-    const parameter = "?category=30b95dde-a820-41dd-b474-902026e3e755";
-    const url = `http://localhost:8080/product${parameter}`;
-    axios
-      .get(url)
-      .then((result) => {
-        // console.log(result.data.data);
-        setProduct(result.data.data);
-        setSearchParams({ category: "NonCoffee" });
-      })
-      .catch((error) => console.error(error));
-  };
-
-  const handleForFoods = () => {
-    const parameter = "?category=ea71bfcd-f1f1-4976-ae1e-9ff0f2c70d0e";
-    const url = `http://localhost:8080/product${parameter}`;
-    axios
-      .get(url)
-      .then((result) => {
-        // console.log(result.data.data);
-        setProduct(result.data.data);
-        setSearchParams({ category: "Foods" });
-      })
-      .catch((error) => console.error(error));
-  };
-  const handleForAllMenu = () => {
-    const url = `http://localhost:8080/product/all`;
-    axios
-      .get(url)
-      .then((result) => {
-        // console.log(result.data.data);
-        // console.log(result.data.data.length);
-        setProduct(result.data.data);
-        setSearchParams();
-      })
-      .catch((error) => console.error(error));
-  };
   useEffect(() => {
     //call action getListProduct
-    console.log("1. use effect component did mount");
     dispatch(getListProducts());
   }, [dispatch]);
   return (
@@ -81,8 +24,8 @@ function Product() {
       <section className="product">
         <div className="container">
           <div className="row">
-            <LeftPanel/>
-            <RightPanel/>
+            <LeftPanel />
+            <RightPanel />
           </div>
         </div>
       </section>
